35ea2d0334584c97665c8331b3b22bdbe4e07bba
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2023-10-28 11:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ const people = [
   },
 ];
 
-// Practicing .map() method
+// Practicing .map() method to extract ages from the 'people' array
 const ages = people.map((person) => {
   return person.age;
 });
 
+// Practicing .map() method to create a new array with modified properties
 const newPeople = people.map((item) => {
   return {
     firstName: item.name,
@@ -33,10 +34,13 @@ const newPeople = people.map((item) => {
   };
 });
 
-//Onto the Web Page
+// Onto the Web Page
+
+
+// Using .map() to create an array of HTML strings with names
 const names = people.map((person) => {
   return `<h2>${person.name}</h2>`;
 });
 
 const div = document.getElementById("result");
-div.innerHTML = names.join("");
+div.innerHTML = names.join(""); // Display the names as HTML content in the 'result' div
